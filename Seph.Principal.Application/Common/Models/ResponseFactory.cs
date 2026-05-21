b951440369052b1eb920ef4e0d9e367b852a7153
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-21 14:55:14 -0600

    fix:documentamos la clase de respuesta.

--- Seph.Principal.Application/Common/Models/ResponseFactory.cs
+++ Seph.Principal.Application/Common/Models/ResponseFactory.cs
@@ -2,6 +2,13 @@
 
 namespace Seph.Principal.Application.Common.Models
 {
+    /*Esta clase se encarga de generar la respuesta del proceso generico, 
+     * es decir, es una clase que se encarga de generar la respuesta de un proceso generico, 
+     * ya sea un command o un query, y esta clase se encarga de generar la respuesta de ese proceso generico,
+     * ya sea una respuesta de exito o una respuesta de error, y esta clase se encarga de generar la respuesta 
+     * de ese proceso generico, ya sea una respuesta de exito o una respuesta de error, 
+     * y esta clase se encarga de generar la respuesta de ese proceso generico,
+     * ya sea una respuesta de exito o una respuesta de error*/
     public static class ResponseFactory
     {
         public static ResponseWrapper<T> Success<T>(T data, string message = "Operación completada correctamente", HttpStatusCode statusCode = HttpStatusCode.OK)
