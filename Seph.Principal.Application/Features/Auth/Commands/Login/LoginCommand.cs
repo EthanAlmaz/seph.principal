93149ca3d371c66d5f45d5b8076e2cead7b2ae4c
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-22 13:19:23 -0600

    feat: se agrega el logincommand para trabajar con  el proceso de login del usuario

---
+++ Seph.Principal.Application/Features/Auth/Commands/Login/LoginCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Application.Features.Auth.DTOs;
+
+namespace Seph.Principal.Application.Features.Auth.Commands.Login
+{
+    public sealed record LoginCommand(string Email, string Password, string DeviceId, string IpAddress):
+        IRequest<ResponseWrapper<AuthResponseDto>>;
+    
+    
+}
