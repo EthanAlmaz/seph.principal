0111632ebbaee685dd10cafa85d42c257e8925e4
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 01:33:25 -0600

    feat:agregamos el nuevo registro que se encarga  de consulta utilizando el aprametro de id

---
+++ Seph.Principal.Application/Features/Users/Queries/GetCurrentUser/GetCurrentUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Application.Features.Auth.DTOs;
+
+namespace Seph.Principal.Application.Features.Users.Queries.GetCurrentUser
+{
+    public sealed record GetCurrentUserQuery(Guid UserId):IRequest<ResponseWrapper<UserSessionDto>>;
+    
+    
+}
