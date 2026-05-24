b893a51876f05e899c92c42ba06f8a80827c5026
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-24 17:12:24 -0600

    feat:terminamos todos los elementos de configuracion base de el repository, aqui se realiza la estrategia para trabajar con los  repositorios de usuario.

---
+++ Seph.Principal.Infraestructure/Persistence/Repositories/RefreshTokenSessionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Seph.Principal.Domain.Entities;
+using Seph.Principal.Domain.Enums;
+using Seph.Principal.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seph.Principal.Infraestructure.Persistence.Repositories
+{
+    public sealed class RefreshTokenSessionRepository(ApplicationDbContext dbContext): IRefreshTokenSessionRepository
+    {
+        public Task<RefreshTokenSession?> GetActiveByTokenHashAsync(string tokenHash, CancellationToken cancellationToken)
+            => dbContext.RefreshTokenSessions
+            .FirstOrDefaultAsync(
+                session => session.TokenHash == tokenHash
+                && session.Status == SessionStatus.Active &&
+                session.ExpiresAtUtc > DateTimeOffset.UtcNow,
+                cancellationToken);
+        public async Task<IReadOnlyList<RefreshTokenSession>> GetActiveByUserIdAsync(Guid userId, CancellationToken cancellationToken)
+        => await dbContext.RefreshTokenSessions
+            .Where(session => session.UserId == userId
+                && session.Status == SessionStatus.Active
+                && session.ExpiresAtUtc > DateTimeOffset.UtcNow)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+        public Task AddAsync(RefreshTokenSession session, CancellationToken cancellationToken)
+        => dbContext.RefreshTokenSessions.AddAsync(session, cancellationToken).AsTask();
+
+        public void Update(RefreshTokenSession session)
+            => dbContext.RefreshTokenSessions.Update(session);
+
+    }
+}
