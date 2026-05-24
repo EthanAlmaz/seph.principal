0ce44fb1677a5559b9e2a9d65bfbf95cc02e194a
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-24 17:24:23 -0600

    feat:se agrega  los servicios necesarios apra trabajar por usuarios concurrentes.

--- Seph.Principal.Application/Common/Interfaces/ICurrentUserService.cs
+++ Seph.Principal.Application/Common/Interfaces/ICurrentUserService.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Seph.Principal.Application.Common.Interfaces
+﻿namespace Seph.Principal.Application.Common.Interfaces
 {
     public interface ICurrentUserService
     {
-        Guid? UserId { get; set; }
-        string? Email { get; set; }
-
-        string? IpAddress { get; set; }
-
-        string? DeviceId { get; set; } 
+        Guid? UserId { get; }
+        string? Email { get; }
+        string? IpAddress { get; }
+        string? DeviceId { get; }
     }
 }
---
+++ Seph.Principal/Services/CurrentUserService.cs
@@ -0,0 +1,23 @@
+﻿using Seph.Principal.Application.Common.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Seph.Principal.Services
+{
+    public sealed class CurrentUserService(IHttpContextAccessor httpContextAccessor) : ICurrentUserService
+    {
+        public Guid? UserId
+        {
+            get
+            {
+                var value = httpContextAccessor.HttpContext?.User.FindFirstValue(JwtRegisteredClaimNames.Sub)
+                    ?? httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                return Guid.TryParse(value, out var userId) ? userId : null;
+            }
+        }
+
+        public string? Email => httpContextAccessor.HttpContext?.User.FindFirstValue(JwtRegisteredClaimNames.Email);
+        public string? IpAddress => httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString();
+        public string? DeviceId => httpContextAccessor.HttpContext?.Request.Headers["X-Device-Id"].FirstOrDefault();
+    }
+}
