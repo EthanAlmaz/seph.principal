7dd3cb6af2b793c0d0df204dfcdb8a1fad416528
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-03 02:26:27 -0600

    fix:modificacion de el metodo handler para retornar la respuesta adecuada.

---
+++ Seph.Principal.Application/Features/Instituciones/Queries/GetInstituciones/GetInstitucionesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Domain.Repositories;
+
+namespace Seph.Principal.Application.Features.Instituciones.Queries.GetInstituciones
+{
+    public sealed class GetInstitucionesQueryHandler(IInstitucionRepository institucionRepository)
+     : IRequestHandler<GetInstitucionesQuery,ResponseWrapper<IReadOnlyList<InstitucionDto>>>
+    {
+        public async Task<ResponseWrapper<IReadOnlyList<InstitucionDto>>>Handle(GetInstitucionesQuery request,CancellationToken cancellationToken)
+        {
+            var instituciones = await institucionRepository.GetAllAsync(cancellationToken);
+
+            IReadOnlyList<InstitucionDto> response = instituciones.Select(x => new InstitucionDto(
+                        x.Id,
+                        x.StrValor,
+                        x.StrDescripcion))
+                    .ToList();
+
+            return ResponseFactory.Success(
+                response,
+                "Instituciones obtenidas correctamente");
+        }
+    }
+}
