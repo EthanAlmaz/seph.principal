123858bb4b18848fa261d1b0dd40d416c6401c2a
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-03 02:05:10 -0600

    feat:creamos el metodo handler para trabajar con la logica del negocio

---
+++ Seph.Principal.Application/Features/Instituciones/Commands/CreateInstitucion/CreateInstitucionCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Interfaces;
+using Seph.Principal.Application.Common.Models;
+using Seph.Principal.Domain.Entities;
+using Seph.Principal.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seph.Principal.Application.Features.Instituciones.Commands.CreateInstitucion
+{
+    public sealed class CreateInstitucionCommandHandler(IInstitucionRepository institucionRepository,
+     IUnitOfWork unitOfWork): IRequestHandler<CreateInstitucionCommand,ResponseWrapper<InstitucionDto>>
+    {
+        public async Task<ResponseWrapper<InstitucionDto>> Handle(CreateInstitucionCommand request,CancellationToken cancellationToken)
+        {
+            var institucion = new Institucion
+            {
+                StrValor = request.StrValor,
+                StrDescripcion = request.StrDescripcion
+            };
+
+            await institucionRepository.AddAsync(institucion,cancellationToken);
+
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            var dto = new InstitucionDto(institucion.Id,institucion.StrValor,institucion.StrDescripcion);
+
+            return ResponseFactory.Success(
+                dto,
+                "Institución registrada correctamente");
+        }
+    }
+}
---
+++ Seph.Principal.Application/Features/Instituciones/Commands/CreateInstitucion/CreateInstitucionCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Seph.Principal.Application.Features.Instituciones.Commands.CreateInstitucion
+{
+    public sealed class CreateInstitucionCommandValidator
+    : AbstractValidator<CreateInstitucionCommand>
+    {
+        public CreateInstitucionCommandValidator()
+        {
+            RuleFor(x => x.StrValor)
+                .NotEmpty()
+                .MaximumLength(150);
+
+            RuleFor(x => x.StrDescripcion)
+                .MaximumLength(450);
+        }
+    }
+}
